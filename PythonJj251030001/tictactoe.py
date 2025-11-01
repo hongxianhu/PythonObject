5f58e1ebe88fb0e3c839310107adca4e0a75e4ed
34797 <3479716187@QQ.com>
2025-11-01 17:45:03 +0800

    tictactoe.py tictactoe_oop.py V1 V1

---
+++ PythonJj251030001/tictactoe.py
@@ -0,0 +1,77 @@
+ALL_SPACES = list("123456789")
+X, O, BLANK = "X", "O", " "
+
+
+def getBlankBoard():
+    board = {}
+    for space in ALL_SPACES:
+        board[space] = BLANK
+    return board
+
+
+def getBoardStr(board):
+    return f"""
+    {board['1']}|{board['2']}|{board['3']} 1 2 3
+    -+-+-
+    {board['4']}|{board['5']}|{board['6']} 4 5 6
+    -+-+-
+    {board['7']}|{board['8']}|{board['9']} 7 8 9"""
+
+
+def isValidSpace(board, space):
+    return space in ALL_SPACES and board[space] == BLANK
+
+
+def isWinner(board, player):
+    b, p = board, player
+    return (
+        (b["1"] == b["2"] == b["3"] == p)
+        or (b["4"] == b["5"] == b["6"] == p)
+        or (b["7"] == b["8"] == b["9"] == p)
+        or (b["1"] == b["4"] == b["7"] == p)
+        or (b["2"] == b["5"] == b["8"] == p)
+        or (b["3"] == b["6"] == b["9"] == p)
+        or (b["3"] == b["5"] == b["7"] == p)
+        or (b["1"] == b["5"] == b["9"] == p)
+    )
+
+
+def isBoardFull(board):
+    for space in ALL_SPACES:
+        if board[space] == BLANK:
+            return False
+    return True
+
+
+def updateBoard(board, space, mark):
+    board[space] = mark
+
+
+def main():
+    print("Welcome to tic-tac-toe!")
+    gameBoard = getBlankBoard()
+    currentPlayer, nextPlayer = X, O
+
+    while True:
+        print(getBoardStr(gameBoard))
+
+        move = None
+        while not isValidSpace(gameBoard, move):
+            print(f"what is {currentPlayer}'s move? (1-9)")
+            move = input()
+        updateBoard(gameBoard, move, currentPlayer)
+
+        if isWinner(gameBoard, currentPlayer):
+            print(getBoardStr(gameBoard))
+            print(currentPlayer + " has won the game!")
+            break
+        elif isBoardFull(gameBoard):
+            print(getBoardStr(gameBoard))
+            print("the game is a tie!")
+            break
+        currentPlayer, nextPlayer = nextPlayer, currentPlayer
+    print("Thanks for playing")
+
+
+if __name__ == "__main__":
+    main()
---
+++ PythonJj251030001/tictactoe_oop.py
@@ -0,0 +1,72 @@
+ALL_SPACES = list("123456789")
+X, O, BLANK = "X", "O", " "
+
+
+class TTTBoard:
+    def __init__(self, usePrettyBoard=False, useLogging=False):
+        self._spaces = {}
+        for space in ALL_SPACES:
+            self._spaces[space] = BLANK
+
+    def getBoardStr(self):
+        return f"""
+        {self._spaces['1']}|{self._spaces['2']}|{self._spaces['3']} 1 2 3
+        -+-+-
+        {self._spaces['4']}|{self._spaces['5']}|{self._spaces['6']} 4 5 6
+        -+-+-
+        {self._spaces['7']}|{self._spaces['8']}|{self._spaces['9']} 7 8 9"""
+
+    def isValidSpace(self, space):
+        return space in ALL_SPACES and self._spaces[space] == BLANK
+
+    def isWinner(self, player):
+        b, p = self._spaces, player
+        return (
+            (b["1"] == b["2"] == b["3"] == p)
+            or (b["4"] == b["5"] == b["6"] == p)
+            or (b["7"] == b["8"] == b["9"] == p)
+            or (b["1"] == b["4"] == b["7"] == p)
+            or (b["2"] == b["5"] == b["8"] == p)
+            or (b["3"] == b["6"] == b["9"] == p)
+            or (b["3"] == b["5"] == b["7"] == p)
+            or (b["1"] == b["5"] == b["9"] == p)
+        )
+
+    def isBoardFull(self):
+        for space in ALL_SPACES:
+            if self._spaces[space] == BLANK:
+                return False
+        return True
+
+    def updateBoard(self, space, mark):
+        self._spaces[space] = mark
+
+
+def main():
+    print("Welcome to tic-tac-toe!")
+    gameBoard = TTTBoard()
+    currentPlayer, nextPlayer = X, O
+
+    while True:
+        print(gameBoard.getBoardStr())
+
+        move = None
+        while not gameBoard.isValidSpace(move):
+            print(f"what is {currentPlayer}'s move? (1-9)")
+            move = input()
+        gameBoard.updateBoard(move, currentPlayer)
+
+        if gameBoard.isWinner(currentPlayer):
+            print(gameBoard.getBoardStr())
+            print(currentPlayer + " has won the game!")
+            break
+        elif gameBoard.isBoardFull():
+            print(gameBoard.getBoardStr())
+            print("the game is a tie!")
+            break
+        currentPlayer, nextPlayer = nextPlayer, currentPlayer
+    print("Thanks for playing")
+
+
+if __name__ == "__main__":
+    main()
