b552470fbb4a4255880eee0f2b1bbb650df70f35
34797 <3479716187@QQ.com>
2025-11-03 09:07:02 +0800

    wizcoin.py v2

--- PythonJj251028001/wizcoin.py
+++ PythonJj251028001/wizcoin.py
@@ -1,11 +1,51 @@
+class WizCoinException(Exception):
+    pass
+
+
 class WizCoin:
     def __init__(self, galleons, sickles, knuts):
-        self.galleons = galleons
-        self.sickles = sickles
-        self.knuts = knuts
+        self._galleons = galleons
+        self._sickles = sickles
+        self._knuts = knuts
 
     def value(self):
-        return (self.galleons * 17 * 29) + (self.sickles * 29) + (self.knuts)
+        return (self._galleons * 17 * 29) + (self._sickles * 29) + (self._knuts)
 
     def weightInGrams(self):
-        return (self.galleons * 31.103) + (self.sickles * 11.34) + (self.knuts * 5.0)
+        return (self._galleons * 31.103) + (self._sickles * 11.34) + (self._knuts * 5.0)
+
+    @property
+    def galleons(self):
+        return self._galleons
+
+    @galleons.setter
+    def galleons(self, value):
+        if not isinstance(value, int):
+            raise f"galleons attr must be set to an int, not a {value.__class__.__qualname__}"
+        if value < 0:
+            raise f"galleons attr must be a positive int, not a {value.__class__.__qualname__}"
+        self._galleons = value
+
+    @property
+    def sickles(self):
+        return self._sickles
+
+    @sickles.setter
+    def sickles(self, value):
+        if not isinstance(value, int):
+            raise f"sickles attr must be set to an int, not a {value.__class__.__qualname__}"
+        if value < 0:
+            raise f"sickles attr must be a positive int, not a {value.__class__.__qualname__}"
+        self._sickles = value
+
+    @property
+    def knuts(self):
+        return self._knuts
+
+    @knuts.setter
+    def knuts(self, value):
+        if not isinstance(value, int):
+            raise f"knuts attr must be set to an int, not a {value.__class__.__qualname__}"
+        if value < 0:
+            raise f"knuts attr must be a positive int, not a {value.__class__.__qualname__}"
+        self._knuts = value
